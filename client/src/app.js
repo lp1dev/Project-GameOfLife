52d46a6cf1536765ee84950fe97652891b3ceeb9
lp1 <mail@lp1.eu>
2018-04-11 22:57:49 +0200

    first commit

---
+++ client/src/app.js
@@ -0,0 +1,35 @@
+/* Projet - Le code du stagiaire 
+~ Lisez le README.md pour plus d'informations ~
+*/
+
+import gameServer from './gameServer'
+
+const seedURL = './seeds/seed1.seed'
+const updateTime = 1000
+
+gameServer.onMessage = (message) => {
+    const messageData = message.data
+        /* 
+           Je comprends pas trop ce que je reçois ici quand 
+           le serveur m'envoie des infos ?! 
+           Ça ressemble à un objet JS mais je peux rien 
+           faire avec... NUL
+        */
+    // console.log('LA DATA', messageData)
+}
+
+gameServer
+    .loadSeed(seedURL)
+    .then((seed) => {
+        /* 
+           Je reçois bien une seed ici !
+        */
+        gameServer.init(seed) // On m'a dit d'utiliser ça mais ça retourne RIEN
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+
+const interval = setInterval(() => {
+    gameServer.next()
+}, updateTime)
